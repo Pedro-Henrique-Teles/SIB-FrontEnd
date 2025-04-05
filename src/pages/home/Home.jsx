4b4ed714cf51c5c6a857072970605ba4c499c4da
pedro-henrique-teles <pedrohenriqueteles732@gmail.com>
2025-04-05 13:44:41 -0300

    Revertendo merge errado da branch main

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,8 @@
-<<<<<<< HEAD
-import Cabecalho from "../cabecalho/Cabecalho";
-import PaginaPrincipal from "../paginaPrincipal/PaginaPrincipal";
-=======
-import { Button } from "@heroui/react";
-import Cabecalho from "../../components/NavBar";
-
 function Home() {
   return (
     <div>
-      <Cabecalho />
       <h1 className="text-6xl text-center bg-black text-white p-7 mt-5">Teste</h1>
-      <Button className="mt-2 text-lg bg-primary" variant="light">
-        Ghost
-      </Button>
     </div>
   );
 }
->>>>>>> parent of 8a1d71e (fix: ajustes finais)
-
-function Home() {
-    return (
-      <>
-        <Cabecalho />
-        <PaginaPrincipal />
-      </>
-    )
-  }
-  
-  export default Home;
+export default Home
